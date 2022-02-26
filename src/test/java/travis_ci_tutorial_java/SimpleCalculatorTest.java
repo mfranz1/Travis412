c6d69d429a89d9b5dd6f112d56f0345d85f251fb
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2022-02-25 20:19:11 -0500

    added subtraction test for TDD

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,10 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	
+	@Test
+	public void testSub() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.sub(5,3)2);
+	}
 }
